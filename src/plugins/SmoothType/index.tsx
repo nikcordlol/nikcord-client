5497bc088451781f3bcc743dba7f5996a358ebfb
nikcordlol <miracgur2000@gmail.com>
2026-05-30 19:42:58 +0300

    Update index.tsx

--- src/plugins/SmoothType/index.tsx
+++ src/plugins/SmoothType/index.tsx
@@ -197,7 +197,7 @@ export default definePlugin({
     name: "SmoothType",
     enabledByDefault: true,
     description: "The plugin allows you to fully customize the cursor caret's visual settings, including adjustable transition delays and custom CSS animation effects.",
-    authors: [Devs.Vendicated],
+    authors: [{ name: "Vendicated", id: 343387112001241089n }],
     tags: ["Nikcord"],
     settings,
 
